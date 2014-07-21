608b7d13115cb4e23a7189233300648284919850
Theo Robbertze <theo.robbertze@gmail.com>
2014-07-21 09:27:29 +0200

    Now working again

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
 if (Meteor.isClient) {
   Meteor.startup(function () {
 
-    var mainCtx = famous.core.Engine.createContext();
+    var Engine = require("famous/core/Engine");
+    var Lightbox = require("famous/views/Lightbox");
+    var Transform = require("famous/core/Transform");
+    var Easing = require("famous/transitions/Easing");
+    var Surface = require("famous/core/Surface");
+    var Scrollview = require("famous/views/Scrollview");
+
+    var mainCtx = Engine.createContext();
     mainCtx.setPerspective(1000);
 
     var lightbox = new famous.views.Lightbox({
       inOpacity: 1,
       outOpacity: 0,
-      inTransform: famous.core.Transform.translate(0, 320, 0),
-      outTransform: famous.core.Transform.translate(0, -320, 1),
-      inTransition: { duration: 400, curve: famous.transitions.Easing.outBack },
-      outTransition: { duration: 400, curve: famous.transitions.Easing.easeOut }
+      inTransform: Transform.translate(0, 320, 0),
+      outTransform: Transform.translate(0, -320, 1),
+      inTransition: { duration: 400, curve: Easing.outBack },
+      outTransition: { duration: 400, curve: Easing.easeOut }
     });
 
     mainCtx.add(lightbox);
 
-    var surface = new famous.core.Surface({
+    var surface = new Surface({
       content: "Hello world",
       size: [200, 200]
     });
 
-    var scrollView = new famous.views.Scrollview();
+    var scrollView = new Scrollview();
     scrollView.sequenceFrom([surface]);
 
     lightbox.show(scrollView);
